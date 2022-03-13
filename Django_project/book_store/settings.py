56cb9f0991ed332523ca60104dbe65d0fcd295ac
f4rk4sh <farkashyaroslav@gmail.com>
2022-03-13 16:07:35 +0200

    adding static, bootstrap, login/logout templates

--- Django_project/book_store/settings.py
+++ Django_project/book_store/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
 
     # Third party apps
 
+    'bootstrapform',
+
 ]
 
 MIDDLEWARE = [
@@ -131,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
